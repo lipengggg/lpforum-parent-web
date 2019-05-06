aa9027b13e4ff97154f8aba6c432cb534e75293b
lipeng <1151449905@qq.com>
2019-05-07 01:15:12 +0800

    主页

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,30 @@
 <template>
   <div id="app">
-    <router-link to = '/'>首页</router-link>
-    <router-link to = '/Shop'>商城</router-link>
-    <router-link to = '/Params/12345/上山打老虎'>动态路由</router-link>
-    <router-link to = '/toHome'>回到首页</router-link>
-    <router-link to = '/all'>所有</router-link>
-    <router-link to = '/count'>计算</router-link>
-    <transition :name="transitionName">
-      <router-view/>
-    </transition>
+    <topmenu></topmenu>
+    <!--<layout></layout>-->
+    <!--<index></index>-->
   </div>
 </template>
 
 <script>
+import topmenu from './components/common/topmenu'
+import layout from './components/common/layout'
+import leftmenu from './components/common/leftmenu'
+import index from './components/index'
+
 export default {
   data () {
-    return {
-      transitionName: 'slide-right'
-    }
+    return {}
   },
-  watch: {
-    $route  (to, form, next) {
-      console.log(to)
-      console.log(form)
-      console.log(next)
-    }
+  components: {
+    topmenu,
+    layout,
+    leftmenu,
+    index
   },
   name: 'App'
 }
 </script>
 
 <style>
-  .slide-left-enter,
-  .slide-right-leave-active {
-    opacity: 0;
-    -webkit-transform: translate(100%, 0);
-    transform: translate(100%, 0);
-  }
-
-  .slide-left-leave-active,
-
-  .slide-right-enter {
-    opacity: 0;
-    -webkit-transform: translate(-100%, 0);
-    transform: translate(-100% 0);
-  }
 </style>
---
+++ src/components/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div id="templatemo_header_panel">
+      <div id="templatemo_title_section">
+        <h1>亚里士多德博客</h1>
+        <span id="mylife">--生活就像海洋，只有意志坚强的人才能到达彼岸！</span></div>
+    </div> <!-- end of templatemo header panel -->
+
+    <div id="templatemo_menu_panel">
+      <div id="templatemo_menu_section">
+        <ul>
+          <li><a href="index.html"  class="current">首页</a></li>
+          <li><a href="#">我的生活</a></li>
+          <li><a href="#">知识库</a></li>
+          <li><a href="#">艺术墙</a></li>
+          <li><a href="#">艺术墙</a></li>
+          <li><a href="#">关于我们</a></li>
+          <li><a href="#">联系我们</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #mylife{
+    color: cornflowerblue;
+  }
+
+  #templatemo_header_panel{
+    width:100%;
+    margin: 0 auto;
+    color: #ffffff;
+    height: 205px;
+  }
+
+  #templatemo_header_panel #templatemo_title_section{
+    width: 100%;
+    height: 132px;	/* +73 ( padding ) */
+    margin:0 auto;
+    padding-top: 73px;
+    text-align: center;
+  }
+
+  #templatemo_menu_panel{
+    width:100%;
+    height: 45px;
+    margin: 0 auto;
+    color: #ffffff;
+  }
+
+  #templatemo_menu_panel #templatemo_menu_section{
+    width: 930px;
+    height: 45px;
+    margin:0 auto;
+    padding: 0 0 0 30px;
+    color: #03a0a6;
+  }
+
+  #templatemo_menu_section ul {
+    float: left;
+    width: 930px;
+    margin: 0;
+    padding: 7px 0 0 0;
+    list-style: none;
+  }
+
+  #templatemo_menu_section ul li{
+    display: inline;
+  }
+
+  #templatemo_menu_section ul li a{
+    float: left;
+    width: 110px;
+    padding: 5px 0;
+    margin-right: 5px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center;
+    text-decoration: none;
+    color: #ffffff;
+    border-bottom: 3px solid #472400;
+  }
+
+  #templatemo_menu_section li a:hover, #templatemo_menu_section li .current{
+    color: #ffffff;
+    border-bottom: 8px solid #703901;
+  }
+</style>
